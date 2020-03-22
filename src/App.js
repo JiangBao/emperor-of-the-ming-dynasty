dda7fac7df169348e60296814d0cf0479b594cba
JiangBao <jiangbao1123@gmail.com>
2020-03-22 09:50:33 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>明朝皇帝那些事</div>
+    );
+  }
+}
+
+export default App;
